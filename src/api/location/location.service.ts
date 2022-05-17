23712cce1693966b9964b96c6f12dfa1910e8831
mitinoh <mite.gj9@gmail.com>
2022-05-17 22:44:23 +0200

    shuffle return data

--- src/api/location/location.service.ts
+++ src/api/location/location.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Inject, Injectable, Logger } from '@nestjs/c
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
-import { LocationDocument , Location} from './entities/location.entity';
+import { LocationDocument, Location } from './entities/location.entity';
 import { Model } from "mongoose";
 import { MongoQueryModel } from 'nest-mongo-query-parser';
 import * as mongoose from 'mongoose'
@@ -11,24 +11,25 @@ import { SaveLocation, SaveLocationDocument } from '../save-location/entities/sa
 import { Document, Schema as MongooseSchema, Types } from "mongoose";
 import { AuthService } from 'src/auth/auth.service';
 import { UserService } from '../user/user.service';
+import shuffleArray from 'src/shared/utils';
 import { of } from 'rxjs';
 
 @Injectable()
 export class LocationService {
 
-  
+
   constructor(
     @InjectModel(Location.name) private locationModel: Model<LocationDocument>,
     @InjectModel(SaveLocation.name) private saveLocationModel: Model<SaveLocationDocument>,
     @Inject('winston') private readonly logger: Logger,
-    private authService: AuthService, 
+    private authService: AuthService,
     private readonly userService: UserService,) { }
 
 
   async create(req: Http2ServerRequest, createLocationDto: CreateLocationDto) {
     try {
-      let newCreateLocation = new this.locationModel({...createLocationDto});
-      
+      let newCreateLocation = new this.locationModel({ ...createLocationDto });
+
       return await newCreateLocation.save();
     } catch (error) {
       this.logger.error(error)
@@ -36,10 +37,10 @@ export class LocationService {
     }
   }
 
-  async findAll(req: Http2ServerRequest,query: MongoQueryModel) {
+  async findAll(req: Http2ServerRequest, query: MongoQueryModel) {
     try {
 
-      
+
       return await this.locationModel
         .find(query.filter)
         .populate('locationCategory')
@@ -47,21 +48,21 @@ export class LocationService {
         .skip(query.skip)
         .sort(query.sort)
         .select(query.select).then(async (locations: Location[]) => {
-          let uid: any =await this.userService.getUserObjectId(req) ?? '';
-        // FIXME: verificare se req contiene token con uid
-          if(uid != ''){
+          let uid: any = await this.userService.getUserObjectId(req) ?? '';
+          // FIXME: verificare se req contiene token con uid
+          if (uid != '') {
             let lData: Location[] = [];
             for (const loc of locations) {
-              let savedLocation: SaveLocation = await this.saveLocationModel.findOne({ location: loc._id.toString(), uid: uid});
+              let savedLocation: SaveLocation = await this.saveLocationModel.findOne({ location: loc._id.toString(), uid: uid });
               loc.saved = (savedLocation != null)
               lData.push(loc)
             }
-           return lData
+            return shuffleArray(lData)
           } else {
-            return locations
+            return shuffleArray(locations)
           }
         })
-        
+
     } catch (error) {
       this.logger.error(error)
       throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
@@ -71,8 +72,8 @@ export class LocationService {
   async findOne(id: string) {
     try {
       return await this.locationModel
-      .findOne({ _id: id })
-      .populate('locationCategory')
+        .findOne({ _id: id })
+        .populate('locationCategory')
     } catch (error) {
       this.logger.error(error)
       throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
@@ -82,10 +83,10 @@ export class LocationService {
   async update(id: string, updateLocationDto: UpdateLocationDto) {
     try {
       return await this.locationModel
-      .findOneAndUpdate(
-        { _id: id },
-        updateLocationDto, 
-        { new: false })
+        .findOneAndUpdate(
+          { _id: id },
+          updateLocationDto,
+          { new: false })
     } catch (error) {
       this.logger.error(error)
       throw new HttpException(error.message, HttpStatus.EXPECTATION_FAILED);
